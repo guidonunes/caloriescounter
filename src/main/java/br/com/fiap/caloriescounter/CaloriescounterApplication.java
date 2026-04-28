fa26a110e2128f318e805fe7552965371b602ea8
Guilherme Nunes <guilherme.augd@gmail.com>
2026-04-28 15:19:07 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/caloriescounter/CaloriescounterApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.caloriescounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriescounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriescounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/caloriescounter/CaloriescounterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.caloriescounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriescounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
