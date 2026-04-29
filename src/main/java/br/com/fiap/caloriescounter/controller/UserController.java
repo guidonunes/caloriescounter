9ef392fc26d443641ba4f04f3a6868491ea65677
Guilherme Nunes <guilherme.augd@gmail.com>
2026-04-29 17:01:51 -0300

    chore: set up user controller paths

---
+++ src/main/java/br/com/fiap/caloriescounter/controller/UserController.java
@@ -0,0 +1,48 @@
+package br.com.fiap.caloriescounter.controller;
+
+
+import br.com.fiap.caloriescounter.model.User;
+import br.com.fiap.caloriescounter.repository.UserRepository;
+import br.com.fiap.caloriescounter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User save(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long userId){
+        userService.deleteUserById(userId);
+    }
+
+    @PutMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public User updateUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
+}
