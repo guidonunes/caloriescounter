8e8b75b4279beeaf5f5e02b414c381103d1936bc
Guilherme Nunes <guilherme.augd@gmail.com>
2026-05-01 16:48:12 -0300

    update: add annotations

--- src/main/java/br/com/fiap/caloriescounter/model/Food.java
+++ src/main/java/br/com/fiap/caloriescounter/model/Food.java
@@ -1,11 +1,37 @@
 package br.com.fiap.caloriescounter.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="TBL_FOOD")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Food {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "SEQ_FOODS"
+    )
+    @SequenceGenerator(
+            name = "SEQ_FOODS",
+            sequenceName = "SEQ_FOODS",
+            allocationSize = 1
+    )
     private Long foodId;
+
     private String name;
+
+    @Column(name = "portion_weight")
     private String portionWeight;
+
     private Double protein;
     private Double carbs;
     private Double fats;
+
+    @Column(name = "total_calories")
     private Double totalCalories;
 }
