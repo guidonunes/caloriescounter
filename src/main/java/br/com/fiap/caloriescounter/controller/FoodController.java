45f60823794d5f5b524206abd489de3746593b30
Guilherme Nunes <guilherme.augd@gmail.com>
2026-05-04 19:45:47 -0300

    chore: implement crud methods

--- src/main/java/br/com/fiap/caloriescounter/controller/FoodController.java
+++ src/main/java/br/com/fiap/caloriescounter/controller/FoodController.java
@@ -5,7 +5,10 @@ import br.com.fiap.caloriescounter.dto.ShowFoodDTO;
 import br.com.fiap.caloriescounter.service.FoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,10 +17,44 @@ public class FoodController {
     @Autowired
     private FoodService foodService;
 
+    //CREATE
     @PostMapping("/foods")
     @ResponseStatus(HttpStatus.CREATED)
     public ShowFoodDTO save(@RequestBody RegisterFoodDTO food) {
         return foodService.saveFood(food);
     }
 
+    //READ
+    @GetMapping("/foods")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ShowFoodDTO> findAll() {
+        return foodService.findAllFoods();
+    }
+
+    @GetMapping("/foods/{foodId}")
+    public ResponseEntity<ShowFoodDTO> findFoodById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(foodService.findFoodById(id));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //UPDATE
+    @PutMapping("/foods")
+    public ResponseEntity<ShowFoodDTO> update(@RequestBody RegisterFoodDTO food) {
+        try {
+            ShowFoodDTO showFoodDTO = foodService.update(food);
+            return ResponseEntity.ok(showFoodDTO);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //DELETE
+    @DeleteMapping("/foods/{foodId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        foodService.delete(id);
+    }
 }
