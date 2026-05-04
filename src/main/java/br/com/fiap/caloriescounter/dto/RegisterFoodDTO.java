d98d22686264bd9a25ac31b9e6deebfef0a008f8
Guilherme Nunes <guilherme.augd@gmail.com>
2026-05-04 15:52:33 -0300

    chore: add record to register new food entries

---
+++ src/main/java/br/com/fiap/caloriescounter/dto/RegisterFoodDTO.java
@@ -0,0 +1,25 @@
+package br.com.fiap.caloriescounter.dto;
+
+import br.com.fiap.caloriescounter.model.Food;
+
+public record RegisterFoodDTO(
+        Long foodId,
+        String name,
+        String portionWeight,
+        Double protein,
+        Double carbs,
+        Double fats,
+        Double totalCalories
+) {
+    public RegisterFoodDTO(Food food) {
+        this (
+                food.getFoodId(),
+                food.getName(),
+                food.getPortionWeight(),
+                food.getProtein(),
+                food.getCarbs(),
+                food.getFats(),
+                food.getTotalCalories()
+        );
+    }
+}
