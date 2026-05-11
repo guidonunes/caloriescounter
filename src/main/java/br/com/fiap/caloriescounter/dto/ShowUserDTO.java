009c5b8776ca2a4e1fe420c7d4d25e678465f66b
Guilherme Nunes <guilherme.augd@gmail.com>
2026-05-11 15:22:52 -0300

    feat: add user role to the response body

--- src/main/java/br/com/fiap/caloriescounter/dto/ShowUserDTO.java
+++ src/main/java/br/com/fiap/caloriescounter/dto/ShowUserDTO.java
@@ -1,17 +1,20 @@
 package br.com.fiap.caloriescounter.dto;
 
 import br.com.fiap.caloriescounter.model.User;
+import br.com.fiap.caloriescounter.model.UserRole;
 
 public record ShowUserDTO(
         Long userId,
         String name,
-        String email
+        String email,
+        UserRole role
 ){
     public ShowUserDTO(User user) {
         this(
                 user.getUserId(),
                 user.getName(),
-                user.getEmail()
+                user.getEmail(),
+                user.getRole()
         );
     }
 
