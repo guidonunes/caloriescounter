9c872c4b69c14be432ccdf9d1864e954c242defc
Guilherme Nunes <guilherme.augd@gmail.com>
2026-05-11 16:00:01 -0300

    feat: implement token validation logic

---
+++ src/main/java/br/com/fiap/caloriescounter/config/security/TokenService.java
@@ -0,0 +1,60 @@
+package br.com.fiap.caloriescounter.config.security;
+
+
+import br.com.fiap.caloriescounter.model.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("my.secret.keyword")
+    private String keyword;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(keyword);
+
+            String token = JWT.create()
+                    .withIssuer("caloriescounter")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Token could not be generated");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(keyword);
+
+            return JWT.require(algorithm)
+                    .withIssuer("caloriescounter")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            return "";
+
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime
+                .now()
+                .plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
